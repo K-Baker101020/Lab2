7322c1119c52ce0a9b283054b88a167b50bfae28
Kristina Baker <BAKERKL1@ETSU.EDU>
2022-09-01 18:28:43 -0400

    //#2 Take a user’s input number and input its multiplication table. The user can choose how high to multiply by.         int num1;
            Console.Write("Enter the first number: ");
            num1 = Convert.ToInt32(Console.ReadLine());
    
            Console.WriteLine($"{num1} * {0} = , {num1} * {1} = ");

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,15 +3,16 @@
         //See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, World");
 
-        //#1 print the output of adding two numbers from user input
-        int num1, num2;
+        //#2 Take a user’s input number and input its multiplication table. The user can choose how high to multiply by.
+        int num1;
+
         Console.Write("Enter the first number: ");
         num1 = Convert.ToInt32(Console.ReadLine());
+        
 
-        Console.Write("Enter the second number");
-        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"{num1} * {0} = , {num1} * {1} = ");
 
-        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+    
     }
 
 }
