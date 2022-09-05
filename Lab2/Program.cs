3a7762e9cc0e2fb897c204337c7102ae1f3dd0cd
K-Baker101020 <bakerkl1@etsu.edu>
2022-09-04 21:32:54 -0400

    public class Program {     public static void Main(string[] args)     {
            //#2 Take a user’s input number and input its multiplication table. The user can choose how high to multiply by.
    
            //Ask player to pick a number
            Console.Write("Pick a number: ");
            int numb = Convert.ToInt16(Console.ReadLine());
    
            //Ask players to choose a max number
            Console.Write("Pick a max number: ");
            int maxNumb = Convert.ToInt16(Console.ReadLine());
            Console.WriteLine(Environment.NewLine);
    
            //For Loop used to create multiplication table
    
            for (int i = 0; i < maxNumb; i++)
            {
                Console.WriteLine(numb + " x " + i + " = " + (numb * i));
    
            }
    
        }
    }

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,18 +1,32 @@
-﻿public class Program {
-    public static void Main(string[] args) {
-        //See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
 
         //#2 Take a user’s input number and input its multiplication table. The user can choose how high to multiply by.
-        int num1;
 
-        Console.Write("Enter the first number: ");
-        num1 = Convert.ToInt32(Console.ReadLine());
-        
 
-        Console.WriteLine($"{num1} * {0} = , {num1} * {1} = ");
+        //Ask player to pick a number
+        Console.Write("Pick a number: ");
+        int numb = Convert.ToInt16(Console.ReadLine());
+
+        //Ask players to choose a max number
+        Console.Write("Pick a max number: ");
+        int maxNumb = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine(Environment.NewLine);
+
+        //For Loop used to create multiplication table
+
+        for (int i = 0; i < maxNumb; i++)
+        {
+            Console.WriteLine(numb + " x " + i + " = " + (numb * i));
+
+        }
+
+    }
+}
 
     
-    }
+    
 
-}
+
