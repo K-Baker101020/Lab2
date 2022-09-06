1db5953a38a16792e05a6c7f6db3cc4791a7f335
K-Baker101020 <bakerkl1@etsu.edu>
2022-09-05 23:58:28 -0400

    //#4 While (action != 5) {             Console.WriteLine("Enter the action to be performed");             Console.WriteLine("Press 1 for Addition");             Console.WriteLine("Press 2 for Subtraction");             Console.WriteLine("Press 3 for Multiplication");             Console.WriteLine("Press 4 for Division");             Console.WriteLine("Press 5 for Modulus");             Console.WriteLine("Press 5 to Exit \n");             int action = Convert.ToInt32(Console.ReadLine());             if (action == 5)             { return; }             Console.WriteLine("Enter 1st input");             int input_1 = Convert.ToInt32(Console.ReadLine());             Console.WriteLine("Enter 2nd input");             int input_2 = Convert.ToInt32(Console.ReadLine());             int result = 0;             switch (action) {                 case 1: {                     result = Addition(input_1, input_2);                     break;                 }                 case 2: {                     result = Subtraction(input_1, input_2);                     break;                 }                 case 3: {                     result = Multiplication(input_1, input_2);                     break;                 }                 case 4: {                     result = Division(input_1, input_2);                     break;                 }                 case 4: {                     result = Modulus(input_1, input_2);                     break;                 }                 default:                     Console.WriteLine("Wrong action!! try again");                     break;             }             Console.WriteLine("The result is {0}", result);             Console.ReadKey();         }         //Addition         public static int Addition(int input_1, int input_2) {             int result = input_1 + input_2;             return result;         }         //Substraction         public static int Subtraction(int input_1, int input_2) {             int result = input_1 + input_2;             return result;         }         //Multiplication         public static int Multiplication(int input_1, int input_2) {             int result = input_1 + input_2;             return result;         }         //Division         public static int Division(int input_1, int input_2) {             int result = input_1 + input_2;             return result;         }         //Modulus         public static int Modulus(int input_1, int input_2) {             int result = input_1 % input_2;             return result;         } }

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,17 +1,79 @@
 ﻿public class Program {
     public static void Main(string[] args) {
-        //See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World");
-
-        //#1 print the output of adding two numbers from user input
-        int num1, num2;
-        Console.Write("Enter the first number: ");
-        num1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter the second number");
-        num2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        
+     //#4
+      While (action != 5)
+        {
+            Console.WriteLine("Enter the action to be performed");  
+            Console.WriteLine("Press 1 for Addition");  
+            Console.WriteLine("Press 2 for Subtraction");  
+            Console.WriteLine("Press 3 for Multiplication");  
+            Console.WriteLine("Press 4 for Division");  
+            Console.WriteLine("Press 5 for Modulus");  
+            Console.WriteLine("Press 5 to Exit \n");  
+            int action = Convert.ToInt32(Console.ReadLine());  
+            if (action == 5)
+            { return; }
+            Console.WriteLine("Enter 1st input");  
+            int input_1 = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("Enter 2nd input");  
+            int input_2 = Convert.ToInt32(Console.ReadLine());  
+            int result = 0;  
+            switch (action) {  
+                case 1: {  
+                    result = Addition(input_1, input_2);  
+                    break;  
+                }  
+                case 2: {  
+                    result = Subtraction(input_1, input_2);  
+                    break;  
+                }  
+                case 3: {  
+                    result = Multiplication(input_1, input_2);  
+                    break;  
+                }  
+                case 4: {  
+                    result = Division(input_1, input_2);  
+                    break;  
+                }  
+                case 4: {  
+                    result = Modulus(input_1, input_2);  
+                    break;  
+                }  
+                default:  
+                    Console.WriteLine("Wrong action!! try again");  
+                    break;  
+            }  
+            Console.WriteLine("The result is {0}", result);  
+            Console.ReadKey();  
+        }  
+        //Addition  
+        public static int Addition(int input_1, int input_2) {  
+            int result = input_1 + input_2;  
+            return result;  
+        }  
+        //Substraction  
+        public static int Subtraction(int input_1, int input_2) {  
+            int result = input_1 + input_2;  
+            return result;  
+        }  
+        //Multiplication  
+        public static int Multiplication(int input_1, int input_2) {  
+            int result = input_1 + input_2;  
+            return result;  
+        }  
+        //Division  
+        public static int Division(int input_1, int input_2) {  
+            int result = input_1 + input_2;  
+            return result;  
+        }  
+        //Modulus  
+        public static int Modulus(int input_1, int input_2) {  
+            int result = input_1 % input_2;  
+            return result;  
+        }  
     }
 
 }
+
+
